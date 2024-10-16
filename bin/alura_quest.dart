b1af9235bab728c517c130da304fdd132f1ad22d
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-10-16 10:06:37 -0400

    feature: Displaying the character sheet

--- bin/alura_quest.dart
+++ bin/alura_quest.dart
@@ -1,12 +1,24 @@
 
 void main() {
-  String name = "Willian Medeiros";
-  String race = "Humanos";
+  String name = "Willian Medeiros,";
+  String race = "Humano";
   String classUser = "Guerreiro";
-  int age = 24;
+  int age = 17;
   double height = 1.80;
   int lifePoints = 100;
-  bool eMagic = true;
+  bool isMagic = true;
 
  List<String> skills = ['Furtividade', "Cura", "Magia de Fogo"];
+
+
+  String infoUser = 'Meu nome é ${name} '
+      'com atlura de ${height}, '
+      '${race} ${age >= 18 ? "maior de idade" : "menor de idade"}, '
+      'da classe de ${classUser}, '
+      'e ${isMagic ? "sou" : "não sou"} mágico.\n'
+      'Possuo ${lifePoints} de pontos de vida.'
+  ;
+
+
+  print(infoUser);
 }
